3b4dd52366b43871da9778a7e906de50a099f69f
RoryWirch <33770280+RoryWirch@users.noreply.github.com>
2020-07-30 23:31:55 -0400

    finished all js and css

--- JS/hw5.js
+++ JS/hw5.js
@@ -7,7 +7,7 @@ Assignment: HW 5
 File: hw5.js
 
 this file contains funtions to validate user input and build a dynamic
-multiplication table from that value
+multiplication table from those values.
 */
 
 function validate(){
@@ -89,18 +89,18 @@ function validate(){
 // clears table before building
 $(document).ready(function(){
     console.log("ready function reached");
-    $("warning_msg").empty()
-    $("#dynamic_table").empty()
+    $("#alert_msg").empty();
+    $("#dynamic_table").empty();
     validate();
 });
 
 
 function make_table() {
     // collect values from the user form
-    var hor_1 = Number(document.getElementById("first_horizontal").value);
-    var hor_2 = Number(document.getElementById("second_horizontal").value);
-    var ver_1 = Number(document.getElementById("first_vertical").value);
-    var ver_2 = Number(document.getElementById("second_vertical").value);
+    var hor_1 = document.getElementById("first_horizontal").value;
+    var hor_2 = document.getElementById("second_horizontal").value;
+    var ver_1 = document.getElementById("first_vertical").value;
+    var ver_2 = document.getElementById("second_vertical").value;
 
     // use console to check values
     console.log("first horizontal value: ", hor_1,
@@ -108,6 +108,94 @@ function make_table() {
                 "first vertical value: ", ver_1,
                 "second vertical value: ", ver_2);
 
+    // clear any old alerts
+    $("#alert_msg").empty();
 
+    // swap first / second number is the first is larger than the second
+    if (hor_1 > hor_2) {
+        var tmp = hor_2;
+        hor_2 = hor_1;
+        hor_1 = tmp;
+        // alert user that the values are being swapped
+        $("#alert_msg").append("<p>The horizontal values have been swapped</p>");
+    }
+
+    if (ver_1 > ver_2) {
+        var tmp = ver_2;
+        ver_2 = ver_1;
+        ver_1 = tmp;
+        // alert user that the values are being swapped
+        $("#alert_msg").append("<p>The vertical values have been swapped</p>");
+    }
+
+    /* ready to build the table, I will be storing values for the table in an
+    object of arrays (rows). These values will be used to create the
+    dynamic multiplication table.
+    */
+    var matrix = {}
+    // calculate number of rows and collumns
+    var num_row = hor_2 - hor_1;
+    var num_col = ver_2 - ver_1;
+    // grab values for the indexes
+    var hor_ind = hor_1;
+    var ver_ind = ver_1;
+
+    // loop to build the table
+    for (var i = 0; i <= num_col; i++){
+        // this temp array will be used to fill in the matrix
+        var tmp = [];
+
+        for (var j = 0; j <= num_row; j++){
+            // find the multiplication value and store it in the temp array
+            var mult_val = hor_ind * ver_ind;
+            tmp[j] = mult_val;
+            hor_ind++;
+        }
+        // store the array (row) in the matrix
+        matrix["row" + i] = tmp;
+        // reset the horizontal index because we are starting a new row
+        hor_ind = hor_1;
+        // increment the vertical index because we are going down a row
+        ver_ind++;
+    }
+
+    /* now we start to add the html content for a table, this will be held in
+    a string called table. first fill out the opening tag and add in the first
+    row and place an empty cell in the first position. some of how I built this
+    code was found here: http://crowdforthink.com/blogs/how-to-create-multiplication-table-using-javascript
+    */
+    var table = "";
+    table += "<table>";
+    table += "<tr><td></td>";
+
+    // fill out the rest of the first row
+    for (var i = hor_1; i <= hor_2; i++) {
+        table += "<td>" + i + "</td>";
+    }
+    // close the first row
+    table += "</tr>";
+
+    // reset the vertical index
+    ver_ind = ver_1;
+
+    // fill in the remaining rows
+    for (var i = 0; i <= num_col; i++){
+        table += "<tr><td>" + ver_ind + "</td>";
+
+        // loop for multiplication values for this row
+        for (var j = 0; j <= num_row; j++){
+            table += "<td>" + matrix["row" + i][j] + "</td>";
+        }
+        ver_ind++;
+
+        table += "</tr>";
+    }
+
+    table += "</table>";
+    console.log(table);
+    // load table into html page
+    //var dyn_table = document.getElementById("#dynamic_table");
+
+    $("#dynamic_table").html(table);
 
 }
