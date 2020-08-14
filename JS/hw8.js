3b499f3c0a98862f063e86d423ea1874483a982f
RoryWirch <33770280+RoryWirch@users.noreply.github.com>
2020-08-14 01:42:29 -0400

    added readme, restored ScrabbleTiles

--- JS/hw8.js
+++ JS/hw8.js
@@ -4,7 +4,9 @@ Contact: rory_wirch@student.uml.edu
 Class: GUI I
 Professor: Wenjin Zhou
 Assignment: HW 8
-File: hw8.html
+File: hw8.js
+
+contains all functions for scrabble
 */
 
 /* global score */
@@ -13,33 +15,33 @@ var global_score = 0;
 /* this data structure is from professor heines, it will be used to keep track
 of the tiles */
 var ScrabbleTiles = [] ;
-ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 1  } ;
-ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 1  } ;
-ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 1  } ;
-ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 1  } ;
-ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 1 } ;
-ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 1  } ;
-ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 1  } ;
-ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 1  } ;
-ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 1  } ;
-ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 0  } ;
-ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 0  } ;
-ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 0  } ;
-ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 0  } ;
-ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 0  } ;
-ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 0  } ;
-ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 0  } ;
-ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 0  } ;
-ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 0  } ;
-ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 0  } ;
-ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 0  } ;
-ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 0  } ;
-ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 0  } ;
-ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 0  } ;
-ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 0  } ;
-ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 0  } ;
-ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 0  } ;
-ScrabbleTiles["Blank"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 0  } ;
+ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12 } ;
+ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3  } ;
+ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8  } ;
+ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2  } ;
 
 /* used when choosing tiles for the board */
 var alpha_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
---
+++ README_SCRABBLE.txt
@@ -0,0 +1,28 @@
+Student: Rory Wirch
+Contact: rory_wirch@student.uml.edu
+Class: GUI I
+Professor: Wenjin Zhou
+Assignment: HW 8
+File: README_SCRABBLE.txt
+
+functionality added:
+    BASIC
+    1.) letter tiles are selected randomly and added to the users hand. this
+        does not use the proper distribution.
+    2.) once letters are placed they cannot be moved.
+    3.) the program keeps track of which tile is placed on which scrabble square.
+    4.) the board has bonus squares and they are used in calculating the Score
+    5.) the score is tallied correctly.
+
+    ADDITIONAL
+    6.) the player can go until all tiles are used, there is a bug where the
+        last tiles do not "leave" the board.
+    7.) the board is cleared after each round.
+    8.) after each hand, only enough tiles to fill the users hand up to 7 tiles
+        are added.
+    9.) score is kept until the end of the game.
+    10.) tiles are only able to be dragged to the board, if dragged elsewhere
+         they are returned to the holder.
+    11.) once placed on the board, tiles cannot be moved.
+    12.) tiles can be placed anywhere, I did not add functionality to only allow
+         pieces to be placed adjacent to previously placed tiles.
